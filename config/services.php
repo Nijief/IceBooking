1d4e47d170fe7778b875cee06867dbfbdcc7a934
Никита <nik.chubeyko@bk.ru>
2026-02-22 13:37:31 +0500

    .env & config-services update

--- config/services.php
+++ config/services.php
@@ -35,4 +35,11 @@ return [
         ],
     ],
 
+    'yookassa' => [
+        'shop_id' => env('YOOKASSA_SHOP_ID'),
+        'secret_key' => env('YOOKASSA_SECRET_KEY'),
+        'return_url' => env('YOOKASSA_RETURN_URL', 'http://localhost/payment/success'),
+        'test_mode' => true,
+    ],
+
 ];
